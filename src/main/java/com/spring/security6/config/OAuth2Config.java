d5d55a68f287b985d56591f86aa5f29d8b4f4e52
nouhailamadrani1 <nouhailamadrani2020@gmail.com>
2024-02-05 08:47:20 +0100

    security

---
+++ src/main/java/com/spring/security6/config/OAuth2Config.java
@@ -0,0 +1,7 @@
+package com.spring.security6.config;
+
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class OAuth2Config {
+}
---
+++ src/main/java/com/spring/security6/entity/TokenType.java
@@ -0,0 +1,5 @@
+package com.spring.security6.entity;
+
+public enum TokenType {
+    BEARER
+}
